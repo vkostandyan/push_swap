fe3ea743ad5ce24298c5f750dabbc00516da85a1
Vitali Kostandyan <vkostand@c2r8s6.42yerevan.am>
2024-05-08 18:10:07 +0400

    updated

--- check_input_utils.c
+++ check_input_utils.c
@@ -6,7 +6,7 @@
 /*   By: vkostand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 16:22:28 by vkostand          #+#    #+#             */
-/*   Updated: 2024/05/02 21:43:08 by vkostand         ###   ########.fr       */
+/*   Updated: 2024/04/27 14:28:04 by vkostand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,21 +54,18 @@ int	is_zero(char *str)
 int	is_number(char *str)
 {
 	int	i;
-	int len;
 
 	i = 0;
 	if (!is_int(str))
 		return (0);
 	if (is_zero(str))
 		return (1);
-	len = ft_strlen(str);
 	if (!ft_isdigit(str[i]) && !ft_issign(str[i]))
 		return (0);
-	len--;
 	i++;
-	while (i < len && ft_isdigit(str[i]))
+	while (str[i] && ft_isdigit(str[i]))
 		i++;
-	if (i != len)
+	if (str[i])
 		return (0);
 	return (1);
 }
